40f85964bd6a262ea822ac04d3e0dbea5b17fa05
AdriFC <adri86vigo@gmail.com>
2021-08-27 14:41:54 +0200

    Configuración Timezone, implementación decimal pipe, currency pipe, percent pipe y fieldset de primeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  nombre: string = 'AdRián';
+export class AppComponent implements OnInit {
+  
+  constructor(private primengConfig: PrimeNGConfig) {
 
-  mostrarNombre() {
-    console.log( this.nombre );
   }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -10,8 +11,10 @@ import { VentasModule } from './ventas/ventas.module';
 
 //Cambiar el locale de la app
 import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
 import { registerLocaleData } from '@angular/common';
 registerLocaleData( localeEs );
+registerLocaleData( localeFr );
 
 
 @NgModule({
@@ -20,6 +23,7 @@ registerLocaleData( localeEs );
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRouterModule,
     SharedModule,
     VentasModule
