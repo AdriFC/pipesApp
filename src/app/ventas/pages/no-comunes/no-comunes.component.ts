89288449113034b2ea02d0216b9e5a1d972fb1fa
AdriFC <adri86vigo@gmail.com>
2021-08-31 13:44:10 +0200

    keyValue pipe, Json pipe y async pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -34,4 +35,36 @@ export class NoComunesComponent  {
     this.clientes.pop();
   }
 
+  //KeyValue Pipe
+  persona = {
+    nombre: 'Adrián',
+    edad: 35,
+    direccion: 'Porriño (Pontevedra)'
+  }
+
+  //Json Pipe
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: true
+    },
+    {
+      nombre: 'Robin',
+      vuela: false
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false
+    },
+  ]
+
+  //Async Pipe
+  miObservable = interval(5000); //0,1,2,3,4,5,6
+
+  valorPromesa = new Promise((resolve, reject) => {
+
+      setTimeout(() => {
+        resolve('Tenemos data de promesa');
+      }, 3500);
+   });
 }
